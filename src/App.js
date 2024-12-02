2511aa3ed426b34808b10f7a14a17a0ed27ea1aa
francisco7j.contacto@gmail.com <francisco7j.contacto@gmail.com>
2024-12-02 14:05:56 -0300

    Archivos Front End Proyecto LookShop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import ProductDetailPrivate from './pages/ProductDetailPrivate'; 
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProductsPrivate from './pages/ProductsPrivate';
+import UploadPublication from './pages/UploadPublication';
+import MyUser from './pages/MyUser'; 
+import Favorites from './pages/Favorites';
+import Messages from './pages/Messages';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/productos" element={<Products />} />
+        <Route path="/producto/:id" element={<ProductDetail />} />
+        <Route path="/producto-privado/:id" element={<ProductDetailPrivate />} /> 
+        <Route path="/ingresa" element={<LoginPage />} />
+        <Route path="/registro" element={<RegisterPage />} />
+        <Route path="/productos-privado" element={<ProductsPrivate />} />
+        <Route path="/subir-publicacion" element={<UploadPublication />} />
+        <Route path="/mi-usuario" element={<MyUser />} /> 
+        <Route path="/favoritos" element={<Favorites />} />
+        <Route path="/mensajes" element={<Messages />} /> 
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
